9c6a8e76c8ab446c77dce97e05f3db8c56823eff
eyac-2017210 <88786551+eyac-2017210@users.noreply.github.com>
2022-07-25 13:08:39 -0600

    se arreglaron algunos errores del front

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -1,28 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
-import { RecetasComponent } from './component/recetas/recetas.component';
-import { MedicamentosComponent } from './component/medicamentos/medicamentos.component';
-import { MisRecetasComponent } from './component/mis-recetas/mis-recetas.component';
 import { LoginComponent } from './component/login/login.component';
-import { RegisterComponent } from './component/register/register.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { RegisterComponent } from './component/register/register.component';
+import { MedicamentosComponent } from './component/medicamentos/medicamentos.component';
+import { RecetasComponent } from './component/recetas/recetas.component';
+import { MisRecetasComponent } from './component/mis-recetas/mis-recetas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    RecetasComponent,
-    MedicamentosComponent,
-    MisRecetasComponent,
     LoginComponent,
+    NavbarComponent,
     RegisterComponent,
-    NavbarComponent
+    MedicamentosComponent,
+    RecetasComponent,
+    MisRecetasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
