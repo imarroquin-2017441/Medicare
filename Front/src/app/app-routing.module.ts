e70674cbaa5fbbc090ebf9cdf01602e8bd02ba47
dsolis-2018306 <dsolis-2018306@kinal.edu.gt>
2022-07-25 18:24:11 -0600

    Se agregaron las vistas con sus respectivos css

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { MedicamentosComponent } from './component/medicamentos/medicamentos.component';
+import { MisRecetasComponent } from './component/mis-recetas/mis-recetas.component';
+import { RecetasComponent } from './component/recetas/recetas.component';
+import { RegisterComponent } from './component/register/register.component';
+
+
+
+
+
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'recetas', component: RecetasComponent},
+  {path: 'medicamentos',component: MedicamentosComponent},
+  {path: 'mis-recetas',component: MisRecetasComponent}
 ];
 
 @NgModule({
